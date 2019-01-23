1eccd41d807c0b6798009bc46b44d65efe4b616b
Michael Portiera <mvportiera@gmail.com>
2019-01-23 19:33:25 +0100

    Group A Addition and Subtraction Tests Added

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,5 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include "math/group_a/item_a.hpp"
+
 #include "util/digest.hpp"
 #include "util/rand.hpp"
---
+++ src/tests/math/group_a/item_a.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "../../catch.hpp"
+
+#include <cinttypes>
+#include <sstream>
+#include <vector>
+#include "../../../source/math/group_a/item_a.hpp"
+
+TEST_CASE("Items in group A should obay modular addition") 
+{
+    dla::item_a x = 54_a;
+    dla::item_a y = 10_a;
+    dla::item_a z = 3_a;
+
+    REQUIRE(x + y == z);
+}
+
+TEST_CASE("Items in group A should obay the commutative property of addition") 
+{
+    dla::item_a x = 54_a;
+    dla::item_a y = 10_a;
+
+    REQUIRE(x + y == y + x);
+}
+
+TEST_CASE("Items in group A should obay the assosiative property of addition") 
+{
+    dla::item_a x = 23_a;
+    dla::item_a y = 32_a;
+    dla::item_a z = 11_a;
+    dla::item_a w = 5_a;
+
+    REQUIRE((x + y) + z == w);
+    REQUIRE(x + (y + z) == w);
+}
+
+TEST_CASE("Items in group A should obay the identity property of addition") 
+{
+    dla::item_a x = 54_a;
+    dla::item_a y = 0_a;
+
+    REQUIRE(x + y == x);
+    REQUIRE(y + x == x);
+}
+
+TEST_CASE("Items in group A should obay modular subtraction") 
+{
+    dla::item_a x = 54_a;
+    dla::item_a y = 10_a;
+    dla::item_a z = 3_a;
+
+    REQUIRE(z - y == x);
+}
+
+TEST_CASE("Items in group A should obay the identity property of subtraction") 
+{
+    dla::item_a x = 54_a;
+    dla::item_a y = 0_a;
+
+    REQUIRE(x - y == x);
+}
