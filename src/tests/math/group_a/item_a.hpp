6d8916dc976b916279e8c64e2111fe73869ffce3
Michael Portiera <mvportiera@gmail.com>
2019-01-23 20:00:27 +0100

    Equality, Negation, and Reciprocal Tests Added

--- src/tests/math/group_a/item_a.hpp
+++ src/tests/math/group_a/item_a.hpp
@@ -7,6 +7,22 @@
 #include <vector>
 #include "../../../source/math/group_a/item_a.hpp"
 
+TEST_CASE("Items in group A that share the same value should be equal") 
+{
+    dla::item_a x = 42_a;
+    dla::item_a y = 42_a;
+
+    REQUIRE(x == y);
+}
+
+TEST_CASE("Items in group A that do not share the same value should not be equal") 
+{
+    dla::item_a x = 42_a;
+    dla::item_a y = 12_a;
+
+    REQUIRE(x != y);
+}
+
 TEST_CASE("Items in group A should obay modular addition") 
 {
     dla::item_a x = 54_a;
@@ -113,4 +129,36 @@ TEST_CASE("Items in group A should obay the identity property of division")
     dla::item_a y = 1_a;
 
     REQUIRE(x / y == x);
+}
+
+TEST_CASE("An item in group A minus itself should equal zero") 
+{
+    dla::item_a x = 42_a;
+    dla::item_a y = 0_a;
+
+    REQUIRE(x - x == y);
+}
+
+TEST_CASE("Adding a negative to an item in group A should be the same as subtracting") 
+{
+    dla::item_a x = 42_a;
+    dla::item_a y = 12_a;
+
+    REQUIRE(x + (-y) == x - y);
+}
+
+TEST_CASE("An item in group A divided by itself should equal one") 
+{
+    dla::item_a x = 42_a;
+    dla::item_a y = 1_a;
+
+    REQUIRE(x / x == y);
+}
+
+TEST_CASE("Multiplying a riciprical by an item in group A should be the same as dividing") 
+{
+    dla::item_a x = 42_a;
+    dla::item_a y = 12_a;
+
+    REQUIRE(x * (!y) == x / y);
 }
