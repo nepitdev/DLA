799b96cf2d42a0a9b340c577ff6d81e5acbc9731
Michael Portiera <mvportiera@gmail.com>
2019-03-10 23:51:37 +0100

    Group A Inverse Table Moved

---
+++ src/source/math/group_a/inverse_table_a.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace dla
+{
+    class inverse_table_a
+    {
+    private:
+        inverse_table_a() {}
+    public:
+        static const uint8_t L[];
+    };
+}
