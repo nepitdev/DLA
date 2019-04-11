21820326a365a8547f48a5c6e0af3e79237b36c6
Michael Portiera <mvportiera@gmail.com>
2019-04-11 19:28:07 +0200

    genkey partially implemented

--- src/source/cli/genkey.cpp
+++ src/source/cli/genkey.cpp
@@ -1,6 +1,30 @@
 #include "genkey.hpp"
 #include "command.hpp"
 
+#include "../math/group_a/item_a.hpp"
+#include "../math/group_a/set_a1.hpp"
+#include "../math/group_a/set_a2.hpp"
+#include "../math/group_a/set_a3.hpp"
+#include "../math/group_a/set_a4.hpp"
+
+#include "../math/group_b/item_b.hpp"
+#include "../math/group_b/set_b1.hpp"
+#include "../math/group_b/set_b2.hpp"
+#include "../math/group_b/set_b3.hpp"
+#include "../math/group_b/set_b4.hpp"
+
+#include "../math/group_c/item_c.hpp"
+#include "../math/group_c/set_c1.hpp"
+#include "../math/group_c/set_c2.hpp"
+#include "../math/group_c/set_c3.hpp"
+#include "../math/group_c/set_c4.hpp"
+
+#include "../math/group_d/item_d.hpp"
+#include "../math/group_d/set_d1.hpp"
+#include "../math/group_d/set_d2.hpp"
+#include "../math/group_d/set_d3.hpp"
+#include "../math/group_d/set_d4.hpp"
+
 namespace dla
 {
     void genkey::verify(command* cmd)
@@ -10,8 +34,8 @@ namespace dla
             cmd->fail("Command 'genkey' requires the flag '-o'");
             return;
         }
-        std::string path = cmd->getFlag("-o");
-        if (path == "")
+        std::string p = cmd->getFlag("-o");
+        if (p == "")
         {
             cmd->fail("Flag '-o' must be followed by a file name");
             return;
@@ -20,6 +44,34 @@ namespace dla
 
     void genkey::exec(command* cmd)
     {
+        rand r;
+        char group = cmd->getFlag("-g").at(0);
+        int size = std::stoi(cmd->getFlag("-s"));
+        std::vector<uint8_t> data;
+
+        switch (group)
+        {
+            case 'a':
+            case 'A':
+                data = buildKeyOfSize<item_a, set_a1, set_a2, set_a3, set_a4>(size);
+                break;
+            case 'b':
+            case 'B':
+                data = buildKeyOfSize<item_b, set_b1, set_b2, set_b3, set_b4>(size);
+                break;
+            case 'c':
+            case 'C':
+                data = buildKeyOfSize<item_c, set_c1, set_c2, set_c3, set_c4>(size);
+                break;
+            case 'd':
+            case 'D':
+                data = buildKeyOfSize<item_d, set_d1, set_d2, set_d3, set_d4>(size);
+                break;
+            default:
+                cmd->fail("Group '" + cmd->getFlag("-g") + "' is not a valid group");
+        }
+        
+
         std::cout << "I dont do anything yet!" << std::endl;
         return;
     }
--- src/source/cli/genkey.hpp
+++ src/source/cli/genkey.hpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include "program.hpp"
 
+#include "../util/rand.hpp"
+#include "../math/matrix.hpp"
+
 namespace dla
 {
     class genkey : public program
@@ -10,5 +13,26 @@ namespace dla
     public:
         virtual void verify(command* cmd);
         virtual void exec(command* cmd);
+    private:
+        template<class I, class S>
+        std::vector<uint8_t> buildKey(int size)
+        {
+            rand r;
+            matrix<I> m(size, size, r);
+            
+            S x;
+            x.setMatrix(m);
+            return x.getFormatedData();
+        }
+
+        template<class I, class S1, class S2, class S3, class S4>
+        std::vector<uint8_t> buildKeyOfSize(int size)
+        {
+            if (size == 8)  return buildKey<I, S1>(8);
+            if (size == 16) return buildKey<I, S2>(16);
+            if (size == 24) return buildKey<I, S3>(24);
+            if (size == 32) return buildKey<I, S4>(32);
+            return std::vector<uint8_t>();
+        }
     };
 }
